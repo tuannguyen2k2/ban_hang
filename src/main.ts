8552a0a03db166f481d17edca0349fe3e81140dc
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-12-21 16:59:56 +0700

    change title swagger

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ export async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     SwaggerModule.setup('api', app, createDocument(app), {
-        customSiteTitle: 'Backend Generator',
+        customSiteTitle: 'Backend Document',
         customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
         customJs: [
             'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
