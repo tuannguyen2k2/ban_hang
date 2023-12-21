2a08d2df545227ffe1bd77a5bf51ed89855e7ef0
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-12-21 16:58:03 +0700

    fix swagger

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { createDocument } from './swagger/swagger';
 export async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
-    app.setGlobalPrefix('/api');
     SwaggerModule.setup('api', app, createDocument(app), {
         customSiteTitle: 'Backend Generator',
         customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
--- src/swagger/swagger.config.ts
+++ src/swagger/swagger.config.ts
@@ -2,8 +2,8 @@
 import { SwaggerConfig } from './swagger.interface';
 
 export const SWAGGER_CONFIG: SwaggerConfig = {
-  title: 'Quan li tuyen dung',
-  description: 'Quan li tuyen dung API',
+  title: 'Bán hàng',
+  description: 'Bán hàng API',
   version: '1.0',
   tags: [],
 };
