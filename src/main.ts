57997d48c8c971922c9a33981c587a9204f8ffce
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-12-21 18:53:57 +0700

    fix dist

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
 import { createDocument } from './swagger/swagger';
 
-export async function bootstrap() {
+async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     SwaggerModule.setup('api', app, createDocument(app), {
