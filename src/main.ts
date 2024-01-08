35a592710b99760cc6208d5ef73efb9da2e72eda
Tuan Nguyen <tuanbmt123123@gmail.com>
2024-01-08 19:25:50 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ export async function bootstrap() {
     // const document = SwaggerModule.createDocument(app, config);
     // SwaggerModule.setup('api', app, document);
     app.enableCors({
-        origin: '*',
+        origin: ['*', 'http://localhost:3000'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true, // Cho phép truy cập có danh tính (đối với các cookie hoặc chứng thực)
     });
